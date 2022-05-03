bc37c780e5a5b321d7bbafacb90ee4a3f8e67b8a
Daniel E Gonzalez <ixa.dgonzalez80@gmail.com>
2022-05-03 11:59:23 -0500

    add caso pi

---
+++ R/0-Lamado-de-paquetes.R
@@ -0,0 +1,22 @@
+# 00-Lamado-de-paquetes.R
+library(mvtnorm)
+library(lattice)
+library(cubature)
+library(scatterplot3d)
+library(RSocrata)
+library(stringr)
+library(BSDA)
+library(summarytools)
+library(psych)
+library(tidyverse)
+library(mvtnorm)
+library(lattice)
+library(cubature)
+library(scatterplot3d)
+library(RSocrata)
+library(stringr)
+library(BSDA)
+library(psych)
+
+
+
